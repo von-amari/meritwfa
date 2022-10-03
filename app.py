b51679521c5888a905d544beb6d9d1d571b0401a
Yvonne K <60331179+von-amari@users.noreply.github.com>
2022-10-03 08:36:55 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def running(proj_id):
     return_dict = {}
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     # browser = webdriver.Chrome(executable_path='C:/bin/chromedriver.exe')
